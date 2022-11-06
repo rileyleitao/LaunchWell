6c7b1a25a2c2cf7d0eb0cf847c94b05c2c3894dc
rleitao-pluto <71091513+rleitao-pluto@users.noreply.github.com>
2022-11-06 15:35:32 -0600

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,32 @@
 <template>
-  <v-app id="inspire">
-    <v-app-bar app color="white" flat>
-      <v-avatar
-        :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
-        size="32"
-      ></v-avatar>
-
-      <v-tabs centered class="ml-n9" color="grey darken-1">
-        <v-tab v-for="link in links" :key="link">
-          {{ link }}
-        </v-tab>
-      </v-tabs>
-
-      <v-avatar
-        class="hidden-sm-and-down"
-        color="grey darken-1 shrink"
-        size="32"
-      ></v-avatar>
-    </v-app-bar>
-
-    <v-main class="grey lighten-3">
-      <v-container>
-        <v-row>
-          <v-col cols="12" sm="10">
-            <v-sheet min-height="70vh" rounded="lg">
-              <!--  -->
-            </v-sheet>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-export default {
-  data: () => ({
-    links: ["How it Works", "Use Cases", "Pricing", "Contact Us"],
-  }),
-};
-</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Signup from '../views/Signup.vue'
-import Parallel from '../views/Parallel.vue'
-import homeassessment from '../views/homeassessment.vue'
-import KelvinElectric from '../views/KelvinElectric.vue'
-import Contact from '../views/Contact.vue'
-// import Splash from '../views/Splash.vue'
-
 
 Vue.use(VueRouter)
 
@@ -24,44 +17,10 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path:'/signup',
-    name:'Signup',
-    component: Signup
-  },
-  {
-    path:'/parallel',
-    name:'Parallel',
-    component: Parallel
-  },
-  {
-    path:'/homeassessment',
-    name:'HomeAssessment',
-    component: homeassessment
-  },
-  {
-    path:'/kelvinelectric',
-    name:'KelvinElectric',
-    component: KelvinElectric
-  },
-  {
-    path:'/contact',
-    name:'Contact',
-    component: Contact
   }
-
-
-
-  // {
-  //   path: '/splash',
-  //   name: 'Splash',
-  // }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes
 })
 
