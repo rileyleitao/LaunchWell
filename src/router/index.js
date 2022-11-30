a2deca85b44c3fbc4aa0d940a9fe89de7aa16c08
rleitao-pluto <71091513+rleitao-pluto@users.noreply.github.com>
2022-11-29 20:34:42 -0600

    initial setup with deployments

--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,44 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Create from '../views/Create.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Create from "../views/Create.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   {
-    path: '/create',
-    name: 'Create',
-    component: Create
+    path: "/create",
+    name: "Create",
+    component: Create,
   },
   {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: Create
+    path: "/dashboard",
+    name: "Dashboard",
+    component: Create,
   },
   {
-  path: '/announcementcreator',
-  name: 'AnnouncementCreator',
+    path: "/announcementcreator",
+    name: "AnnouncementCreator",
   },
-]
-
+];
+//history to remove the #//
 const router = new VueRouter({
-  routes
-})
+  mode: "history",
+  routes,
+});
 
-export default router
+export default router;
